2e09cb3c99459be1990d12cd9e9d3fc86788469c
FaithMso <>
2022-10-18 13:38:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+age = 30
+
+print(age)
+
+age = 40
+
+print(age)
+
+age = 35  #integer
+PI = 3.14  #float
+
+maths_operations = 1 + 3 * 4 /2 -2
+
+print(maths_operations)
+
+float_division = 13/5
+print(float_division)
+
+integer_division = 13 //5
+
+
+print(integer_division)
+
+remainder = 13 % 5 #modulus for finding the reminder
+
+print(remainder)
+
+x = 37
+
+remainder_1 = x % 2  #finding odd numbers
+
+print(remainder_1)
+
+my_string = "Hello, World!"
+
+print(my_string)
+
+string_with_quote = "Hello, it's me."
+
+another_string_with_quotes = 'He said "you are amazing" yesterday.'
+
+multiline ="" "Hello, World. My name is Faith. Welcome to my code."""
+
+print(multiline)
+
+print(string_with_quote)
+
+print(another_string_with_quotes)
+
+name = "Faith"
+greeting = "Hello, " + name 
+
+print(greeting)
+
